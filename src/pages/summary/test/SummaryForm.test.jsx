c8f77f5d6f338cbccfbfdd4d55a39faf53e4a1e5
Soundhar <laptopisasset@gmail.com>
2021-06-20 20:03:11 +0530

    Added all the basic summary.

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<SummaryForm />);
+
+  const checkBox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+
+  const confirmButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+
+  fireEvent.click(checkBox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkBox);
+  expect(confirmButton).toBeDisabled();
+});
