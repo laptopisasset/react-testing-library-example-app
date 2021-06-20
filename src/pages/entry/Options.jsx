2ee8b9efc4ce64be7800e6db0285ff33e8e591f1
Soundhar <laptopisasset@gmail.com>
2021-06-21 02:44:35 +0530

    Added toppings option.

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,19 +3,20 @@ import axios from "axios";
 import Row from "react-bootstrap/Row";
 
 import ScoopOptions from "./ScoopOptions";
+import ToppingOptions from "./ToppingOptions";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`http://localhost:3030/scoops`)
+      .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch(console.error);
   }, [optionType]);
 
   // TODO: replace `null` with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
 
   const optionItems = items.map((item, index) => (
     <ItemComponent key={index} {...item} />
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -14,3 +14,20 @@ test("displays image for each scoop option from server", async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("displays image for each toppings option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingsImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingsImages).toHaveLength(3);
+
+  const altText = toppingsImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
