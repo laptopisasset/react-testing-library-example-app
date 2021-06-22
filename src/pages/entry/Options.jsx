27dd5016103088afb52537c892855b463711b1f1
Soundhar <laptopisasset@gmail.com>
2021-06-22 08:37:52 +0530

    Added the code for handling error.

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,16 +4,22 @@ import Row from "react-bootstrap/Row";
 
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch(console.error);
+      .catch(setError);
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
